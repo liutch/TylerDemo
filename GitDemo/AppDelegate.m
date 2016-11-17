e53f4a2814c6530d75112e81a77ca49b8190c373
TylerLiu <liutch@126.com>
2016-11-17 09:29:41 +0800

    stash

--- GitDemo/AppDelegate.m
+++ GitDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"===修改了一半");
     return YES;
 }
 
